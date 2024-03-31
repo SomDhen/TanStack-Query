a29ab49fe0c7fee6720732035b491ec4836c2899
Sonam Dhendup <somdhendup@gmail.com>
2024-03-31 21:55:18 +0900

    Setting a React/TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
-import "./index.css";
+// import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
